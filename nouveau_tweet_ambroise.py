9669f3ec374cc10817df85f11beb85a07cbc85c5
TimFaverjon <tim.faverjon@student-cs.fr>
2019-06-17 16:33:56 +0200

    tropstyle

--- nouveau_tweet_ambroise.py
+++ nouveau_tweet_ambroise.py
@@ -8,6 +8,8 @@ import json
 from textblob import TextBlob
 import ast
 
+import twitter_connection_setup as tcs
+
 def is_in_doc(doc,nb):
     with open(doc,"r") as document :
         req = document.readlines()
@@ -51,13 +53,7 @@ def note_sentiment(doc):
     return positif, negatif, nb_positif, nb_negatif, nb_neutre
 
 #Connexion à l'API
-consumer_key = "Lvj8ytu4zOTTrbS7KsI8i89T2"
-consumer_secret = "UCOwM9mlt2Mljl4zRAPrDT8mPjuZrJQu37hdkcsBEbwX2SJSnt"
-access_key="1139588267819618304-d8R3qmYSlPYpASwNLiG0o8reYEOP6l"
-access_secret="XcANXc8nhYLGkWH1qK8WJ4tDf9BDFGdWF6Rr3cwkDBoe8"
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api=tweepy.API(auth)
+api=tcs.twitter_setup()
 
 #ouverture des deux fichiers utiles : celui qui recense les tweets sous format json; celui qui recense les id pour ne pas avoir de doublons
 fichier = open("twitt.txt", "a")
