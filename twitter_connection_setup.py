1708123f9f3506f9a656f20c27c492e171ffdf84
TimFaverjon <45000561+TimFaverjon@users.noreply.github.com>
2019-06-17 11:46:42 +0200

    Add files via upload

--- twitter_connection_setup.py
+++ twitter_connection_setup.py
@@ -9,10 +9,13 @@ access_key="1139588267819618304-d8R3qmYSlPYpASwNLiG0o8reYEOP6l"
 access_secret="XcANXc8nhYLGkWH1qK8WJ4tDf9BDFGdWF6Rr3cwkDBoe8"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-api=tweepy.API(auth)
+
+def twitter_setup():
+    api=tweepy.API(auth)
+    return(api)
 
 
 
 
 # confirm account being used for OAuth
-print ("API NAME IS: ", api.me().name)
+#print ("API NAME IS: ", api.me().name)
